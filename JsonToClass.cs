bc06477246e3453042c39dc2c178d2ca3ae82b94
nilke1995 <brekhin@bk.ru>
2017-12-03 14:37:03 +0300

    add files

---
+++ JsonToClass.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace twitter_bot
+{
+    public class JsonToClass
+    {
+        public class Type
+        {
+            public string name { get; set; }
+            public string code { get; set; }
+        }
+
+        public class Medium
+        {
+            public string name { get; set; }
+            public Type type { get; set; }
+            public bool active { get; set; }
+            public string address { get; set; }
+            public object website { get; set; }
+            public string reg_date { get; set; }
+            public string reg_num { get; set; }
+        }
+
+        public class MoneyTransfersSumByType
+        {
+            public double Contract { get; set; }
+            public double Grant { get; set; }
+            public double Subsidy { get; set; }
+        }
+
+        public class Contract
+        {
+            public string reg_num { get; set; }
+            public string date { get; set; }
+            public List<string> description { get; set; }
+            public double amount { get; set; }
+            public string customer { get; set; }
+        }
+
+        public class Subsidie
+        {
+            public string date { get; set; }
+            public string name { get; set; }
+            public double amount { get; set; }
+            public string customer { get; set; }
+            public string customer_inn { get; set; }
+            public string customer_kpp { get; set; }
+        }
+
+        public class Grant
+        {
+            public string date { get; set; }
+            public List<string> description { get; set; }
+            public string source_of_modey { get; set; }
+            public string url { get; set; }
+        }
+
+
+        public class Result
+        {
+            public string name { get; set; }
+            public string inn { get; set; }
+            public List<Medium> media { get; set; }
+            public double money_transfers_sum { get; set; }
+            public MoneyTransfersSumByType money_transfers_sum_by_type { get; set; }
+            public List<Contract> contracts { get; set; }
+            public List<object> subsidies { get; set; }
+            public List<object> grants { get; set; }
+        }
+
+        public class RootObject
+        {
+            public int count { get; set; }
+            public string next { get; set; }
+            public string previous { get; set; }
+            public List<Result> results { get; set; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TweetSharp;
+using static twitter_bot.JsonToClass;
+
+namespace twitter_bot
+{
+    class Program
+    {
+        private static string cons_key = "v9hhw9Has5ggDVC63K3rnby1v";
+        private static string cons_secret = "DoScc4lt710QGXdQPXqB7KiaN8QTkugRpAgnJhjmpFydkfdm4G";
+        private static string access_token = "849806977760886785-w26rUBLGnBkFak7IWuT446O0M23zKOU";
+        private static string secret_access = "necG42Cvstq5hNSdihMrqr4tcDsXpCrIwLV0wDM6Yg7f5";
+        public static string datetime = $"[{DateTime.Now.ToShortTimeString()}]";
+        public static TwitterService twitter = new TwitterService(cons_key, cons_secret, access_token, secret_access);
+        private static RootObject info;
+
+        public static JObject getInfo()
+        {
+            WebRequest reqGet = WebRequest.Create("https://openmassmedia.ru/api/founders/");
+            WebResponse resp = reqGet.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string s = sr.ReadToEnd();
+            JObject parsed = JObject.Parse(s);
+            return parsed;
+        }
+
+
+        static void Main(string[] args)
+        {
+            run();
+        }
+        public static void run()
+        {
+        Start:
+            Console.WriteLine("input");
+            int count = 0;
+            JObject jp = getInfo();
+            string result;
+            //--------------------------------
+            string json = jp.ToString();
+            double contractsSum = 0;
+            info = JsonConvert.DeserializeObject<RootObject>(json);
+
+            //for(int i = 0; i < info.)
+            foreach (Result r in info.results)
+            {
+                Console.WriteLine(info.results.Count.ToString());
+                try
+                {
+                    foreach (Contract c in info.results[0].contracts)
+                    {
+                        contractsSum += c.amount;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+                Console.WriteLine("Учредитель " + r.name + " заключил контракт на сумму " + info.results[0].contracts[0].amount.ToString());
+                sendTweet("Учредитель " + r.name + " заключил контракт на сумму " + info.results[0].contracts[0].amount.ToString());
+                Thread.Sleep(10000);
+            }
+            //--------------------------------
+            
+            count++;
+                
+          //  }
+            Thread.Sleep(4 * 60 * 60 * 1000);
+            goto Start;
+        }
+
+        public static void sendTweet(string _status)
+        {
+            twitter.SendTweet(new SendTweetOptions { Status = _status }, (tweet, response) =>
+            {
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+                    Console.WriteLine("not OK");
+                }
+            });
+        }
+    }
+}
